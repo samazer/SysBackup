292e602ce712fcfcb0909243595a5190a574d7d4
Sam Azer <sam@azertech.net>
2012-01-10 15:16:41 -0500

    major improvements & code cleanup, some bugs found and fixed, full rotation implementation completed, switched from phpdoc to doxygen (but did not update the comments - lots fo work remaining to be done.)

---
+++ doc/doxygen/html/navtree.js
@@ -0,0 +1,377 @@
+var NAVTREE =
+[
+  [ "SysBackup", "index.html", [
+    [ "Related Pages", "pages.html", [
+      [ "Todo List", "todo.html", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "account_info_c", "classaccount__info__c.html", null ],
+      [ "account_migration_c", "classaccount__migration__c.html", null ],
+      [ "applog_c", "classapplog__c.html", null ],
+      [ "backupvfs_backupset_c", "classbackupvfs__backupset__c.html", null ],
+      [ "backupvfs_baseio_c", "classbackupvfs__baseio__c.html", null ],
+      [ "backupvfs_fileio_c", "classbackupvfs__fileio__c.html", null ],
+      [ "backupvfs_hashlist_c", "classbackupvfs__hashlist__c.html", null ],
+      [ "backupvfs_localio_c", "classbackupvfs__localio__c.html", null ],
+      [ "backupvfs_localio_rotation_c", "classbackupvfs__localio__rotation__c.html", null ],
+      [ "backupvfs_main_c", "classbackupvfs__main__c.html", null ],
+      [ "backupvfs_sshremote_c", "classbackupvfs__sshremote__c.html", null ],
+      [ "backupvfs_sshremote_rotation_c", "classbackupvfs__sshremote__rotation__c.html", null ],
+      [ "backupvfs_stats_c", "classbackupvfs__stats__c.html", null ],
+      [ "effective_user_c", "classeffective__user__c.html", null ],
+      [ "linux_distro_c", "classlinux__distro__c.html", null ],
+      [ "localexec_c", "classlocalexec__c.html", null ],
+      [ "logfile_c", "classlogfile__c.html", null ],
+      [ "mktarball_util_c", "classmktarball__util__c.html", null ],
+      [ "named_parameters_base_c", "classnamed__parameters__base__c.html", null ],
+      [ "named_parameters_c", "classnamed__parameters__c.html", null ],
+      [ "plugin_appreference_c", "classplugin__appreference__c.html", null ],
+      [ "plugin_base_c", "classplugin__base__c.html", null ],
+      [ "plugin_framework_c", "classplugin__framework__c.html", null ],
+      [ "remote_exec_c", "classremote__exec__c.html", null ],
+      [ "remote_fileio_c", "classremote__fileio__c.html", null ],
+      [ "remote_filesystem_c", "classremote__filesystem__c.html", null ],
+      [ "stopwatch_c", "classstopwatch__c.html", null ],
+      [ "sysbackup_plugin_base_c", "classsysbackup__plugin__base__c.html", null ],
+      [ "system_backup_app_c", "classsystem__backup__app__c.html", null ],
+      [ "teelog_c", "classteelog__c.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "account_info_c", "classaccount__info__c.html", [
+        [ "account_migration_c", "classaccount__migration__c.html", null ]
+      ] ],
+      [ "backupvfs_hashlist_c", "classbackupvfs__hashlist__c.html", null ],
+      [ "backupvfs_stats_c", "classbackupvfs__stats__c.html", null ],
+      [ "effective_user_c", "classeffective__user__c.html", null ],
+      [ "linux_distro_c", "classlinux__distro__c.html", null ],
+      [ "logfile_c", "classlogfile__c.html", null ],
+      [ "mktarball_util_c", "classmktarball__util__c.html", null ],
+      [ "named_parameters_base_c", "classnamed__parameters__base__c.html", [
+        [ "named_parameters_c", "classnamed__parameters__c.html", null ]
+      ] ],
+      [ "plugin_appreference_c", "classplugin__appreference__c.html", [
+        [ "backupvfs_backupset_c", "classbackupvfs__backupset__c.html", null ],
+        [ "plugin_base_c", "classplugin__base__c.html", [
+          [ "backupvfs_fileio_c", "classbackupvfs__fileio__c.html", [
+            [ "backupvfs_baseio_c", "classbackupvfs__baseio__c.html", [
+              [ "backupvfs_localio_c", "classbackupvfs__localio__c.html", [
+                [ "backupvfs_localio_rotation_c", "classbackupvfs__localio__rotation__c.html", null ]
+              ] ],
+              [ "backupvfs_sshremote_c", "classbackupvfs__sshremote__c.html", [
+                [ "backupvfs_sshremote_rotation_c", "classbackupvfs__sshremote__rotation__c.html", null ]
+              ] ]
+            ] ]
+          ] ],
+          [ "sysbackup_plugin_base_c", "classsysbackup__plugin__base__c.html", null ]
+        ] ]
+      ] ],
+      [ "remote_filesystem_c", "classremote__filesystem__c.html", [
+        [ "remote_fileio_c", "classremote__fileio__c.html", null ]
+      ] ],
+      [ "stopwatch_c", "classstopwatch__c.html", [
+        [ "applog_c", "classapplog__c.html", [
+          [ "teelog_c", "classteelog__c.html", [
+            [ "localexec_c", "classlocalexec__c.html", null ],
+            [ "plugin_framework_c", "classplugin__framework__c.html", [
+              [ "backupvfs_main_c", "classbackupvfs__main__c.html", [
+                [ "system_backup_app_c", "classsystem__backup__app__c.html", null ]
+              ] ]
+            ] ],
+            [ "remote_exec_c", "classremote__exec__c.html", null ]
+          ] ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Data Fields", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "aforms", "namespaceaforms.html", null ],
+      [ "server", "namespaceserver.html", null ],
+      [ "sysbackup", "namespacesysbackup.html", null ]
+    ] ],
+    [ "File List", "files.html", [
+      [ "dumpusers", "dumpusers.html", null ],
+      [ "localexec", "localexec.html", null ],
+      [ "remexec", "remexec.html", null ],
+      [ "sysbackup", "sysbackup.html", null ],
+      [ "sysbackup.conf", "sysbackup_8conf.html", null ],
+      [ "app/sysbackup-plugins.inc", "sysbackup-plugins_8inc.html", null ],
+      [ "app/sysbackup-vfs-backupset.inc", "sysbackup-vfs-backupset_8inc.html", null ],
+      [ "app/sysbackup-vfs-base.inc", "sysbackup-vfs-base_8inc.html", null ],
+      [ "app/sysbackup-vfs-hashlists.inc", "sysbackup-vfs-hashlists_8inc.html", null ],
+      [ "app/sysbackup-vfs-io.inc", "sysbackup-vfs-io_8inc.html", null ],
+      [ "app/sysbackup-vfs-localio-rotation.inc", "sysbackup-vfs-localio-rotation_8inc.html", null ],
+      [ "app/sysbackup-vfs-localio.inc", "sysbackup-vfs-localio_8inc.html", null ],
+      [ "app/sysbackup-vfs-sshremote-rotation.inc", "sysbackup-vfs-sshremote-rotation_8inc.html", null ],
+      [ "app/sysbackup-vfs-sshremote.inc", "sysbackup-vfs-sshremote_8inc.html", null ],
+      [ "app/sysbackup-vfs.inc", "sysbackup-vfs_8inc.html", null ],
+      [ "app/sysbackup.inc", "sysbackup_8inc.html", null ],
+      [ "lib/general-log.inc", "general-log_8inc.html", null ],
+      [ "lib/general-namedparameters.inc", "general-namedparameters_8inc.html", null ],
+      [ "lib/general-net.inc", "general-net_8inc.html", null ],
+      [ "lib/general-plugins.inc", "general-plugins_8inc.html", null ],
+      [ "lib/general-stopwatch.inc", "general-stopwatch_8inc.html", null ],
+      [ "lib/general-sysexec.inc", "general-sysexec_8inc.html", null ],
+      [ "lib/general-sysutils.inc", "general-sysutils_8inc.html", null ],
+      [ "lib/general-tee.inc", "general-tee_8inc.html", null ],
+      [ "lib/general-utils.inc", "general-utils_8inc.html", null ],
+      [ "lib/sys-account-info.inc", "sys-account-info_8inc.html", null ],
+      [ "lib/sys-account-migration.inc", "sys-account-migration_8inc.html", null ],
+      [ "lib/sys-packagelist.inc", "sys-packagelist_8inc.html", null ],
+      [ "lib/sys-remote-fileio.inc", "sys-remote-fileio_8inc.html", null ],
+      [ "lib/sys-remote-filesys.inc", "sys-remote-filesys_8inc.html", null ],
+      [ "lib/sys-tarballs.inc", "sys-tarballs_8inc.html", null ]
+    ] ],
+    [ "Directories", "dirs.html", [
+      [ "app", "dir_7c3f8a30817664b13e60a1ccacefa6f4.html", null ],
+      [ "lib", "dir_e1b14e73bf661583829f9e5382b32873.html", null ]
+    ] ],
+    [ "Globals", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
